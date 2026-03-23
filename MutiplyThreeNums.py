71da948ff52853acafe5970a89322f9cb558a02d
XueKaizhi <20119470@setu.ie>
2026-03-23 08:35:57 +0800

    Add MutiplyThreeNums.py

---
+++ MutiplyThreeNums.py
@@ -0,0 +1,10 @@
+#Please input the numbers
+num1=float(input("Please input the first number:"))
+num2=float(input("Please input the second number"))
+num3=float(input("Please input the third number"))
+
+#calculate the number
+product= num1*num2*num3
+
+#print the outcome
+print ("The three numbers are {0} {1} {2},and the mutiply is {3}".format(num1,num2,num3,product))
